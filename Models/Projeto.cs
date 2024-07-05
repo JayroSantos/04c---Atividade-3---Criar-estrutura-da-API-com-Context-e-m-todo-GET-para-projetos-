d5b80a02f5607d131af7dda01bb560e82aa07830
Jayro <144926964+JayroSantos@users.noreply.github.com>
2024-07-05 17:02:57 -0300

    Add files via upload

---
+++ Models/Projeto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exo.WebApi.Models
+{
+    public class Projeto
+    {
+        public int Id { get; set; }
+        public string NomeDoProjeto { get; set; }
+        public string Area { get; set; }
+        public bool Status { get; set; }
+    }
+}
